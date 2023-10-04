db75181d7d83ad107b439e2d54d0a8e0583268d2
kaihzk <103640495+kaihzk@users.noreply.github.com>
2023-10-05 10:53:53 +1100

    Added more elements in Forgot page

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 # the website is created under /show/info, and its relationship with index
 # if users access the website /show/info, it auto executes index
 @app.route("/show/login")
-def index():
+def get_login():
     # render_template is a file for Flask to open automatically and run,
     # then return the information to user
     # it will go to templates file to find index by default
-    return render_template("index.html")
+    return render_template("login.html")
+
+
+@app.route("/show/Forgot")
+def get_password():
+    return render_template("ForgotPassword.html");
+
 
 if __name__ == "__main__":
     app.run()
